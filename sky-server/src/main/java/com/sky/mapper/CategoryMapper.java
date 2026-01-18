554fbb3d2ee72816be86088b6fa5aa3ea13707fc
xiaowei <3220468644@QQ.com>
2026-01-18 10:55:35 +0800

    菜品分类功能完善

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -1,15 +1,40 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 public interface CategoryMapper {
 
 
+    //分页查询菜品分类
     Page<Category> sreachCategroy(CategoryPageQueryDTO categoryPageQueryDTO);
+
+
+    //新增分类
+    @Insert("insert into category (type,name,sort,status,create_time,update_time,create_user,update_user)" +
+            " values (#{type},#{name},#{sort},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    void addCategory(Category category);
+
+    //删除菜品分类
+    @Delete("delete from category where  id = #{id}")
+    void delCategory(long id);
+
+
+
+    //修改菜品状态和信息
+    void updateCateInfo(Category category);
+
+
+    //根据类型查询菜品分类信息
+    List<Category> sreachCatebyType(CategoryPageQueryDTO categoryPageQueryDTO);
 }
--- sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -2,18 +2,23 @@ package com.sky.service.impl;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.sky.context.BaseContext;
+import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
 import com.sky.mapper.CategoryMapper;
 import com.sky.result.PageResult;
 import com.sky.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.awt.geom.PathIterator;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
+@Slf4j
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
@@ -32,11 +37,68 @@ public class CategoryServiceImpl implements CategoryService {
         List<Category> categoryList = categories.getResult();
 
         return  new PageResult(total ,categoryList);
+    }
 
+    //新增菜品分类
+    @Override
+    public void addCategory(CategoryDTO categoryDTO) {
 
+        Category category = new Category();
+        category.setName(categoryDTO.getName());
+        category.setSort(categoryDTO.getSort());
+        category.setType(categoryDTO.getType());
+        category.setStatus(0);
+        category.setCreateTime(LocalDateTime.now());
+        category.setCreateTime(LocalDateTime.now());
+        category.setCreateUser(BaseContext.getCurrentId());
+        category.setUpdateUser(BaseContext.getCurrentId());
 
+        categoryMapper.addCategory(category);
 
+    }
 
+    @Override
+    public void delCategory(long id) {
+        categoryMapper.delCategory(id);
+    }
+
+    @Override
+    public void updateCateInfo(long id, int status) {
+        Category category = Category.builder()
+                .id(id)
+                .status(status)
+                .updateTime(LocalDateTime.now())
+                .updateUser(BaseContext.getCurrentId())
+                .build();
+
+        categoryMapper.updateCateInfo(category);
+    }
+
+    @Override
+    public void updateCateInfo(CategoryDTO categoryDTO) {
+        Category categoryinfo = Category.builder()
+                .id(categoryDTO.getId())
+                .type(categoryDTO.getType())
+                .name(categoryDTO.getName())
+                .sort(categoryDTO.getSort())
+                .updateTime(LocalDateTime.now())
+                .updateUser(BaseContext.getCurrentId())
+                .build();
+
+        categoryMapper.updateCateInfo(categoryinfo);
+    }
+
+    @Override
+    public PageResult SreachCatebyType(CategoryPageQueryDTO categoryPageQueryDTO) {
+
+        //修改page
+        Integer page = (categoryPageQueryDTO.getPage()-1)*categoryPageQueryDTO.getPageSize();
+        categoryPageQueryDTO.setPage(page);
+        List<Category> catelist = categoryMapper.sreachCatebyType(categoryPageQueryDTO);
+        PageResult pageResult = new PageResult();
+        pageResult.setRecords(catelist);
+        log.info("查询后参数:{}",catelist);
+        return pageResult;
 
     }
 }
